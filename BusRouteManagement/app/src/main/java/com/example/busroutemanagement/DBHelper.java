cb6feee454a328aa0dbd2d8da03d079f8da4734a
Meraj al Maksud <merajmasuk@gmail.com>
2023-10-11 17:45:55 +0600

    form creation

---
+++ BusRouteManagement/app/src/main/java/com/example/busroutemanagement/DBHelper.java
@@ -0,0 +1,32 @@
+package com.example.busroutemanagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "routedb";
+    private static final int DB_VERSION = 1;
+    private static final String TABLE_NAME = "routes";
+    private static final String ID_COL = "id";
+    private static final String BUSTOP_COL = "bustop";
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + BUSTOP_COL + " TEXT)";
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
