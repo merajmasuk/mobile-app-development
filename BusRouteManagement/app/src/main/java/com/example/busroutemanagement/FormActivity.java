59e0ad0b6e39978e89874f4c7f0655b8167be565
Meraj al Maksud <merajmasuk@gmail.com>
2023-10-12 01:52:48 +0600

    result page

---
+++ BusRouteManagement/app/src/main/java/com/example/busroutemanagement/FormActivity.java
@@ -0,0 +1,44 @@
+package com.example.busroutemanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FormActivity extends AppCompatActivity {
+    Button btnSubmit;
+    TextView tvStart, tvDest, tvDateTime;
+    EditText edtStart, edtDest, edtDateTime;
+
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        tvShow = findViewById(R.id.tvShow);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvStart = findViewById(R.id.tvStart);
+        tvDest = findViewById(R.id.tvDest);
+        tvDateTime = findViewById(R.id.tvDateTime);
+        edtStart = findViewById(R.id.edtStart);
+        edtDest = findViewById(R.id.edtDest);
+        edtDateTime = findViewById(R.id.edtDateTime);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormActivity.this, MainActivity.class);
+                intent.putExtra("txtStart", edtStart.getText().toString());
+                intent.putExtra("txtDest", edtDest.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
--- BusRouteManagement/app/src/main/java/com/example/busroutemanagement/MainActivity.java
+++ BusRouteManagement/app/src/main/java/com/example/busroutemanagement/MainActivity.java
@@ -3,38 +3,20 @@ package com.example.busroutemanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSubmit;
-    TextView tvStart, tvDest, tvDateTime;
-    EditText edtStart, edtDest, edtDateTime;
-
-    TextView tvShow;
+    TextView tvStart, tvDest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvShow = findViewById(R.id.tvShow);
-
-        btnSubmit = findViewById(R.id.btnSubmit);
         tvStart = findViewById(R.id.tvStart);
         tvDest = findViewById(R.id.tvDest);
-        tvDateTime = findViewById(R.id.tvDateTime);
-        edtStart = findViewById(R.id.edtStart);
-        edtDest = findViewById(R.id.edtDest);
-        edtDateTime = findViewById(R.id.edtDateTime);
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvShow.setText("Button works!");
-            }
-        });
+        tvStart.setText(getIntent().getStringExtra("txtStart"));
+        tvDest.setText(getIntent().getStringExtra("txtDest"));
     }
 }
